7f26983365a482531f707705499eead8acbf54fe
Alex Pak <alexpak@limsi.fr>
2011-09-07 18:18:01 +0200

    Implemented CYK parser + building and printing tree. Todo: build forests

--- src/parsers/cyk.py
+++ src/parsers/cyk.py
@@ -9,12 +9,15 @@ from collections import defaultdict
 class CYK:
     literals = set()
     nonterminal = set()
-    rules = defaultdict(list)
+    rules = []
+    
+    index = defaultdict(set)
+    rindex = defaultdict(set)
     
     def __init__(self, grammar):
-        candidates= set()
+        candidates = set()
         for rule in grammar:
-            self.rules[rule[0]].append(rule[1:])
+            self.rules.append(rule)
             self.nonterminal.add(rule[0])
             
             if len(rule) == 2:
@@ -22,8 +25,98 @@ class CYK:
                 
         self.literals = candidates - self.nonterminal
         
+        for n in range(0, len(self.rules)):
+            rule = self.rules[n]
+            if len(rule) == 2:
+                self.rindex[rule[1]].add(n)
+                
+            self.index[rule[0]].add(n)
+            
     def tokenize(self, str):
         return str.split(' ')
 
     def parse(self, str):
         tokens = self.tokenize(str)
+        
+        P = {};
+        len_tokens = len(tokens)
+        
+        # returns positions of matching components or empty list
+        def match(rule, start, length):
+            if len(rule) == 1:
+                result = [length] if rule[0] in P[start][length] else []
+            else:
+                result = []
+                for l in range(1, length):
+                    if start + l > len_tokens:
+                        break
+
+                    if rule[0] not in P[start][l]:
+                        continue
+                    
+                    tail = match(rule[1:], start + l, length - l)
+                    if not len(tail):
+                        continue
+                    result = [l] + tail
+                    break
+                    
+            return result
+        
+        # start at the leafs
+        for p in range(0, len_tokens):
+            tokenset = set(tokens[p])
+            P[p] = {1: defaultdict(set)}
+            while len(tokenset):
+                new_tokenset = set()
+                for token in tokenset:
+                    for n in self.rindex[token]:
+                        rule = self.rules[n]
+                        P[p][1][rule[0]].add(n)
+                        new_tokenset.add(rule[0])
+                        
+                tokenset = new_tokenset
+                    
+        for l in range(2, len_tokens + 1): # length
+            for p in range(0, len_tokens): # position
+                P[p][l] = defaultdict(set)
+                for n in range(0, len(self.rules)):
+                    rule = self.rules[n]
+                    matching = match(rule[1:], p, l)
+                    if matching:
+                        P[p][l][rule[0]].add((n, tuple(matching)))
+        
+        self.P = P
+        self.tokens = tokens
+        
+        return len(P[0][len_tokens])
+    
+    def build_tree(self):
+        def build(head, start, length):
+            for matching in self.P[start][length][head]:
+                if type(matching) == tuple:
+                    rule_n, lengths = matching
+                    rule = self.rules[rule_n]
+                    root = [head]
+                    start = start
+                    for i in range(0, len(rule) - 1):
+                        root.append(build(rule[i + 1], start, lengths[i]))
+                        start += lengths[i]
+                else:
+                    rule = self.rules[matching]
+                    if rule[1] in self.literals:
+                        root = [head, rule[1]]
+                    else:
+                        root = [head, build(rule[1], start, length)]
+                    
+                return tuple(root)
+            
+        return build('EX', 0, len(self.tokens))
+    
+    def print_tree(self, tree, padding = '', pad_with = '\t'):
+        if len(tree) == 2 and tree[1] in self.literals:
+            print('{0}({1} "{2}")'.format(padding, tree[0], tree[1]))
+        else:
+            print('{0}({1}'.format(padding, tree[0]))
+            for branch in tree[1:]:
+                self.print_tree(branch, padding + pad_with, pad_with)
+            print('{0})'.format(padding))
--- test/test-cyk.py
+++ test/test-cyk.py
@@ -12,14 +12,32 @@ import parsers
 from parsers.cyk import CYK
 
 rules = '''
-    EX  = "(" EX ")" | N
+    EX  = B1 EX B2 | N
+    B1  = "("
+    B2  = ")"
     EX  = EX OP2 EX | OP1 EX
     N   = "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "0"
     OP2 = "+" | "-" | "/" | "*"
     OP1 = "+" | "-"
 '''
 
+#rules = '''
+#    EX  = EX OP EX | N
+#    N   = "1" | "2" | "3"
+#    OP = "+" | "-"
+#'''
+
 grammar = parsers.read_rules(rules)
 
 parser = CYK(grammar)
-#parser.parse('1 + 2 * ( 3 - 6 )')
+#result = parser.parse('1 + 2 * ( 3 - 6 ) + 9 + 0 / 2')
+result = parser.parse('1 + 2 + 3')
+#parser.parse('1 + 2')
+tree = parser.build_tree()
+parser.print_tree(tree)
+
+'''
+(EX (EX 1)
+    (OP +)
+    (N 2))
+'''
